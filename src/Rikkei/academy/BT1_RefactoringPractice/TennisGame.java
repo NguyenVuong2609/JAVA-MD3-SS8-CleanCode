1276b3687e193ac613abd613335b7f801fb71a3f
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-13 14:33:56 +0700

    1303

--- src/Rikkei/academy/BT1_RefactoringPractice/TennisGame.java
+++ src/Rikkei/academy/BT1_RefactoringPractice/TennisGame.java
@@ -2,76 +2,54 @@ package Rikkei.academy.BT1_RefactoringPractice;
 
 public class TennisGame {
 
-    public static final String ADVANTAGE_PLAYER_1 = "Advantage player1";
-    public static final String ADVANTAGE_PLAYER_2 = "Advantage player2";
-    public static final String WIN_FOR_PLAYER_1 = "Win for player1";
-    public static final String WIN_FOR_PLAYER_2 = "Win for player2";
+    public static final String ADVANTAGE = "Advantage for ";
+
     public static final String LOVE = "Love";
     public static final String FIFTEEN = "Fifteen";
     public static final String THIRTY = "Thirty";
     public static final String FORTY = "Forty";
-    public static final String LOVE_ALL = "Love-All";
-    public static final String FIFTEEN_ALL = "Fifteen-All";
-    public static final String THIRTY_ALL = "Thirty-All";
-    public static final String FORTY_ALL = "Forty-All";
     public static final String DEUCE = "Deuce";
 
-    public static String getScore(int firstOperandScore, int secondOperandScore) {
+    public static String getScore(int firstPlayerScore, int secondPlayerScore) {
         String score = "";
-        int tempScore = 0;
-        if (firstOperandScore == secondOperandScore) {
-            switch (firstOperandScore) {
-                case 0:
-                    score = LOVE_ALL;
-                    break;
-                case 1:
-                    score = FIFTEEN_ALL;
-                    break;
-                case 2:
-                    score = THIRTY_ALL;
-                    break;
-                case 3:
-                    score = FORTY_ALL;
-                    break;
-                default:
-                    score = DEUCE;
-                    break;
+        if (firstPlayerScore == secondPlayerScore) {
+            if (firstPlayerScore < 4) {
+                score = getScoreString(firstPlayerScore) + "_ALL";
+            } else {
+                score = DEUCE;
             }
-        } else if (firstOperandScore >= 4 || secondOperandScore >= 4) {
-             score = getMinusResult(firstOperandScore, secondOperandScore);
+        } else if (firstPlayerScore > 3 || secondPlayerScore > 3) {
+            int minusScore = firstPlayerScore - secondPlayerScore;
+            if (Math.abs(minusScore) == 1) {
+                score = ADVANTAGE + ((minusScore > 0) ? "Player 1" : "Player 2");
+            } else {
+                score = ((minusScore > 0) ? "Player 1" : "Player 2") + "Win!";
+            }
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = firstOperandScore;
-                else {
-                    score += "-";
-                    tempScore = secondOperandScore;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += LOVE;
-                        break;
-                    case 1:
-                        score += FIFTEEN;
-                        break;
-                    case 2:
-                        score += THIRTY;
-                        break;
-                    case 3:
-                        score += FORTY;
-                        break;
-                }
-            }
+            score = getScoreString(firstPlayerScore) + " - " + getScoreString(secondPlayerScore);
         }
         return score;
     }
 
-    private static String getMinusResult(int firstOperandScore, int secondOperandScore) {
-        int minusResult = firstOperandScore - secondOperandScore;
-        String score;
-        if (minusResult == 1) score = ADVANTAGE_PLAYER_1;
-        else if (minusResult == -1) score = ADVANTAGE_PLAYER_2;
-        else if (minusResult >= 2) score = WIN_FOR_PLAYER_1;
-        else score = WIN_FOR_PLAYER_2;
-        return score;
+
+    private static String getScoreString(int score) {
+        String scoreToString = "";
+        switch (score) {
+            case 0:
+                scoreToString = LOVE;
+                break;
+            case 1:
+                scoreToString = FIFTEEN;
+                break;
+            case 2:
+                scoreToString = THIRTY;
+                break;
+            case 3:
+                scoreToString = FORTY;
+                break;
+            default:
+                break;
+        }
+        return scoreToString;
     }
 }
